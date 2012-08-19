10cc7af612725f7f88a1d6e8cfc2c0784d931486
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2012-08-19 12:26:50 +0400

    Complete sample

--- XmlParserGen/CodeDomHelpers.cs
+++ XmlParserGen/CodeDomHelpers.cs
@@ -43,6 +43,38 @@ namespace XmlParserGen {
         public static void AddParameter<TParameter>(this CodeMemberMethod method, string name) {
             method.Parameters.Add(new CodeParameterDeclarationExpression(typeof(TParameter), name));
         }
+        public static void Return(this CodeMemberMethod method, string type) {
+            method.ReturnType = new CodeTypeReference(type);
+        }
+    }
+
+    static class CodeTypeReferenceExpressionExtensions {
+        public static CodeMethodInvokeExpression Invoke(this CodeTypeReferenceExpression type, string name,
+            params CodeExpression[] parameters) {
+            return new CodeMethodInvokeExpression(type, name, parameters);
+        }
+        public static CodePropertyReferenceExpression Get(this CodeTypeReferenceExpression type, string name) {
+            return new CodePropertyReferenceExpression(type, name);
+        }
+    }
+
+    static class CodeVariableReferenceExpressionExtensions {
+        public static CodeMethodInvokeExpression Invoke(this CodeVariableReferenceExpression variable, string name) {
+            return new CodeMethodInvokeExpression(variable, name, new CodeExpression[0]);
+        }
+        public static CodeMethodInvokeExpression Invoke(this CodeVariableReferenceExpression variable, string name,
+            params CodeExpression[] parameters) {
+            return new CodeMethodInvokeExpression(variable, name, parameters);
+        }
+        public static CodePropertyReferenceExpression Get(this CodeVariableReferenceExpression variable, string name) {
+            return new CodePropertyReferenceExpression(variable, name);
+        }
+    }
+
+    static class CodeMethodInvokeExpressionExtensions {
+        public static CodePropertyReferenceExpression Get(this CodeMethodInvokeExpression invocation, string name) {
+            return new CodePropertyReferenceExpression(invocation, name);
+        }
     }
 
     static class CodeDom {
@@ -67,8 +99,37 @@ namespace XmlParserGen {
                 method.Attributes |= MemberAttributes.Public;
             return method;
         }
+        public static CodeConstructor CreateConstructor(bool @public = true) {
+			CodeConstructor constructor = new CodeConstructor {
+				Attributes = MemberAttributes.Final
+			};
+            if(@public)
+                constructor.Attributes |= MemberAttributes.Public;
+            return constructor;
+        }
         public static CodeVariableDeclarationStatement DeclareVariable<T>(CodeExpression expr) {
             return new CodeVariableDeclarationStatement(typeof(T), GetVariableName(typeof(T).Name), expr);
+        }
+        public static CodeVariableReferenceExpression VarRef(string name) {
+            return new CodeVariableReferenceExpression(name);
+        }
+        public static CodeVariableReferenceExpression VarRef(CodeVariableDeclarationStatement declaration) {
+            return VarRef(declaration.Name);
+        }
+        public static CodeFieldReferenceExpression FieldRef(string name) {
+            return new CodeFieldReferenceExpression(new CodeThisReferenceExpression(), name);
+        }
+        public static CodeTypeReferenceExpression TypeRef<T>() {
+            return new CodeTypeReferenceExpression(typeof(T));
+        }
+        public static CodeObjectCreateExpression New<T>(params CodeExpression[] parameters) {
+            return new CodeObjectCreateExpression(typeof(T), parameters);
+        }
+        public static CodeObjectCreateExpression New(string name, params CodeExpression[] parameters) {
+            return new CodeObjectCreateExpression(name, parameters);
+        }
+        public static CodeMethodReturnStatement Return(CodeExpression expression) {
+            return new CodeMethodReturnStatement(expression);
         }
     }
 }
--- XmlParserGen/Main.cs
+++ XmlParserGen/Main.cs
@@ -3,6 +3,7 @@ using System.CodeDom;
 using Microsoft.CSharp;
 using System.CodeDom.Compiler;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 
@@ -30,33 +31,48 @@ namespace XmlParserGen {
 
             CodeTypeDeclaration authorType = CodeDom.CreateClass("Author");
             authorType.AddProperty<string>("Name");
+            GenerateConstructor(authorType);
             ns.Types.Add(authorType);
             rootType.AddProperty(authorType.Name);
 
-            CodeConstructor constr = new CodeConstructor();
-            constr.Attributes = MemberAttributes.Final;
-            constr.Parameters.Add(new CodeParameterDeclarationExpression("XElement", "element"));
-            rootType.Members.Add(constr);
+            GenerateConstructor(rootType, @public: false);
 
-            var readFromFileMethod = NewMethod(rootType);
+            var readFromFileMethod = CreateReadFromFileMethod(rootType);
             rootType.Members.Add(readFromFileMethod);
 
             ns.Types.Add(rootType);
         }
-        static CodeMemberMethod NewMethod(CodeTypeDeclaration rootType) {
+        static void GenerateConstructor(CodeTypeDeclaration authorType, bool @public = true) {
+            CodeConstructor authorCtor = CodeDom.CreateConstructor(@public);
+            authorCtor.AddParameter<XElement>("element");
+            foreach(var field in authorType.Members.OfType<CodeMemberField>()) {
+                CodeExpression fieldInitializeExression;
+                if(field.Type.BaseType.StartsWith("System.")) //TODO: rewrite to get rid of this check
+                    fieldInitializeExression = CodeDom.VarRef("element").Invoke("Element",
+                        new CodePrimitiveExpression(field.Name)).Get("Value");
+                else
+                    fieldInitializeExression = CodeDom.New(field.Type.BaseType,
+                        CodeDom.VarRef("element").Invoke("Element", new CodePrimitiveExpression(field.Name)));
+                var assignment = new CodeAssignStatement(CodeDom.FieldRef(field.Name),
+                    fieldInitializeExression);
+                authorCtor.Statements.Add(assignment);
+            }
+            authorType.Members.Add(authorCtor);
+        }
+        static CodeMemberMethod CreateReadFromFileMethod(CodeTypeDeclaration rootType) {
             CodeMemberMethod readFromFileMethod = CodeDom.CreateStaticMethod("ReadFromFile", rootType.Name);
             string filenameParameterName = "filename";
             readFromFileMethod.AddParameter<string>(filenameParameterName);
-            var fileOpenExpression = new CodeObjectCreateExpression(typeof(FileStream), new CodeVariableReferenceExpression(filenameParameterName), new CodePropertyReferenceExpression(new CodeTypeReferenceExpression(typeof(FileMode)), "Open"));
-            var newStreamStatement = CodeDom.DeclareVariable<Stream>(fileOpenExpression);
-            readFromFileMethod.Statements.Add(newStreamStatement);
+            var fileOpenExpression = CodeDom.New<FileStream>(CodeDom.VarRef(filenameParameterName), CodeDom.TypeRef<FileMode>().Get("Open"));
+            var stream = CodeDom.DeclareVariable<Stream>(fileOpenExpression);
+            readFromFileMethod.Statements.Add(stream);
             var tryFinally = new CodeTryCatchFinallyStatement();
-            var loadXDocumentExpression = new CodeMethodInvokeExpression(new CodeTypeReferenceExpression(typeof(XDocument)), "Load", new CodeVariableReferenceExpression(newStreamStatement.Name));
-            var newXDocument = CodeDom.DeclareVariable<XDocument>(loadXDocumentExpression);
-            tryFinally.TryStatements.Add(newXDocument);
-            var returnNewRootObject = new CodeMethodReturnStatement(new CodeObjectCreateExpression(rootType.Name, new CodePropertyReferenceExpression(new CodeVariableReferenceExpression(newXDocument.Name), "Root")));
+            var loadXDocumentExpression = CodeDom.TypeRef<XDocument>().Invoke("Load", CodeDom.VarRef(stream));
+            var xDocument = CodeDom.DeclareVariable<XDocument>(loadXDocumentExpression);
+            tryFinally.TryStatements.Add(xDocument);
+            var returnNewRootObject = CodeDom.Return(CodeDom.New(rootType.Name, CodeDom.VarRef(xDocument).Get("Root")));
             tryFinally.TryStatements.Add(returnNewRootObject);
-            var disposeStreamExpression = new CodeMethodInvokeExpression(new CodeVariableReferenceExpression(newStreamStatement.Name), "Dispose", new CodeExpression[0]);
+            var disposeStreamExpression = CodeDom.VarRef(stream).Invoke("Dispose");
             tryFinally.FinallyStatements.Add(disposeStreamExpression);
             readFromFileMethod.Statements.Add(tryFinally);
             return readFromFileMethod;
